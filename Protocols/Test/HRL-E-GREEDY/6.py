6f55c7598729e0f2317eb63c0fafc8af66cdd419
lorenzo <lorenzosteccanella@gmail.com>
2019-11-11 22:21:30 +0100

    changes in protocol

--- Protocols/Test/HRL-E-GREEDY/6.py
+++ Protocols/Test/HRL-E-GREEDY/6.py
@@ -72,7 +72,7 @@ class variables():
             "weight_mse": 0.5,
             "weight_ce_exploration": 0.01,
             "learning_rate": 0.0001,
-            "gamma": 0.95,
+            "gamma": 0.99,
             "batch_size": 6,
             "preprocessing": preprocessing
         }
