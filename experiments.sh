632e755da560be1322120b0f6a4a2e8cd3fca697
lorenzo <lorenzosteccanella@gmail.com>
2019-11-11 18:46:20 +0100

    modified sh files

--- experiments.sh
+++ experiments.sh
@@ -5,3 +5,10 @@ python main.py Test.HRL-E-GREEDY.4 &&
 python main.py Test.HRL-E-GREEDY.5 &&
 python main.py Test.HRL-E-GREEDY.6 &&
 python main.py Test.A2C.1
+python main.py Test_SIL.HRL-E-GREEDY.1 &&
+python main.py Test_SIL.HRL-E-GREEDY.2 &&
+python main.py Test_SIL.HRL-E-GREEDY.3 &&
+python main.py Test_SIL.HRL-E-GREEDY.4 &&
+python main.py Test_SIL.HRL-E-GREEDY.5 &&
+python main.py Test_SIL.HRL-E-GREEDY.6 &&
+python main.py Test_SIL.A2C_SIL.1
--- experiments2.sh
+++ experiments2.sh
@@ -5,3 +5,10 @@ python main.py Test.HRL-PSEUDO-COUNT.4 &&
 python main.py Test.HRL-PSEUDO-COUNT.5 &&
 python main.py Test.HRL-PSEUDO-COUNT.6 &&
 python main.py Test.A2C.2
+python main.py Test_SIL.HRL-PSEUDO-COUNT.1 &&
+python main.py Test_SIL.HRL-PSEUDO-COUNT.2 &&
+python main.py Test_SIL.HRL-PSEUDO-COUNT.3 &&
+python main.py Test_SIL.HRL-PSEUDO-COUNT.4 &&
+python main.py Test_SIL.HRL-PSEUDO-COUNT.5 &&
+python main.py Test_SIL.HRL-PSEUDO-COUNT.6 &&
+python main.py Test_SIL.A2C_SIL.2
