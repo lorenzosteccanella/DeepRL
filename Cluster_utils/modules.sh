4b82c6d975dcf62ced78cb5f884f3d26848fc10a
Lorenzo STECCANELLA <lsteccanella@login01>
2020-05-31 17:35:40 +0200

    added cluster utils

---
+++ Cluster_utils/modules.sh
@@ -0,0 +1,5 @@
+#module load Tensorflow/1.14.0-foss-2017a-Python-3.6.4
+#module load h5py/2.7.0-foss-2017a-Python-3.6.4
+module load Tkinter/3.6.4-foss-2017a-Python-3.6.4
+module load libGLU/9.0.0-foss-2017a
+
---
+++ Cluster_utils/run.sh
@@ -0,0 +1,13 @@
+E_PATH='/homedtic/lsteccanella/DeepRL/Protocols/TEST/SIL_POSITION_TRANSFER_2/'
+prefix='/homedtic/lsteccanella/DeepRL/Protocols/'
+suffix=
+for i in $(find $E_PATH -name '*.py' -type f);
+do 
+	PROT_PATH=${i#"$prefix"}
+	PROT_PATH=${PROT_PATH%.*}
+	PROT_PATH="$(tr / . <<<$PROT_PATH)"
+	sbatch /homedtic/lsteccanella/DeepRL/Cluster_utils/run-sbatch.sh $PROT_PATH
+	
+done
+
+
